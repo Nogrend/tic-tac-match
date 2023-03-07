eda289de8eb5db54b06325fe02c3f825c790c8b2
BOUB <bram.bouwens@witteveenbos.com>
2023-03-07 18:15:19 +0100

    first phase; using joystick

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <LedControl.h>
+
+LedControl ledMaxtrixA = LedControl(12,11,10,1);
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,3 +10,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+
+/*
+- create a joystick instance
+- create a led matrix instance
+- move one led with joystick up, down, right, left
+- constant one direction, led move one unit per half second
+*/
